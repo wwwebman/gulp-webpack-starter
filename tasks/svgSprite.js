c0aca71df5675fa63921ed95801982cedb93e173
thismandima <dima@25wat.com>
2017-02-21 18:38:27 +0100

    checkout gulp-svg-sprites > gulp-svg-sprite

--- tasks/svgSprite.js
+++ tasks/svgSprite.js
@@ -8,28 +8,49 @@ var reload      = browserSync.reload;
 var imagemin    = require ('gulp-imagemin');
 var notify      = require("gulp-notify");
 var plumber     = require('gulp-plumber');
-var svgSprite   = require('gulp-svg-sprites');
+var svgSprite   = require('gulp-svg-sprite');
 var path        = require("path");
 
 var config      = require("../config");
 var mode        = require("./helpers/mode");
 
-gulp.task("svg", function(){
-	return gulp.src(path.join(config.root.dev, config.svg.dev, '*.svg'))
-    .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
-    .pipe(svgSprite({
-        /**
-         * We need to Put CSS file in dev/sass/vandor/_sprite for bundling
-         * @type - fix
-         */
-        cssFile: path.join('../../../../', config.root.dev, config.css.dev, 'parts/vendor/_sprite.scss'),
-        baseSize: 16,
-        preview: false,
-        svgPath: '../img/svg/%f',
-        svg: {
-            sprite: "sprite.svg"
+
+var svgConf = {
+    shape: {
+        spacing: { 
+            padding: 10
+        },
+        dimension       : {
+            maxWidth    : 32,
+            maxHeight   : 32
+        },
+    },
+    mode: {
+        css: {
+            dimensions: true,
+            common: 'icon',
+            layout: "vertical",
+            prefix: '.icon-',
+            bust: false,
+            dest : './',
+            sprite: '../svg/sprite.svg',
+            render: {
+                scss: {
+                    /**
+                     * Put SCSS file in dev/sass/vandor/_sprite to make concatenation > main.css
+                     * @type - fix
+                     */
+                    dest : path.join('../../../../', config.root.dev, config.css.dev, 'parts/vendor/_sprite.scss'),
+                }
+            }
         }
-    }))
+    }
+}
+
+gulp.task("svg", function(){
+    return gulp.src(path.join(config.root.dev, config.svg.dev, '*.svg'))
+    .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
+    .pipe(svgSprite(svgConf))
     .pipe(gulp.dest(path.join(config.root.dist, config.svg.dist)))
     .pipe(reload({stream: true}));
 });
