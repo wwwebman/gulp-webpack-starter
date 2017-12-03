b6a514ea35de4cfa3351b8a581621a0f3e322676
webman <hello@webman.pro>
2017-12-03 16:14:50 +0100

    Change scripts naming

--- tasks/helpers/mode.js
+++ tasks/helpers/mode.js
@@ -1,14 +1,3 @@
-/**
- * Return true if ENV is --production
- * @returns {object}
- */
-
-var gutil = require('gulp-util');
-
 module.exports = {
-	production: gutil.env.production,
-	show: function(){
-		var info = this.production ? 'Production Mode' : 'Development Mode';
-		console.log("\n[ " + info + " Launched ]\n");
-	}
+	production: process.env.NODE_ENV === 'production' ? true : false,
 }
