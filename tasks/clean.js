b95a395d2071bc79134177aaa130992254bf11f5
Dmytro Chumak <dmytro.chumak@equiqo.com>
2020-01-25 20:48:01 +0100

    [dangerous] Fix clean tag to make possible clean in out folder

--- tasks/clean.js
+++ tasks/clean.js
@@ -7,9 +7,15 @@ const del = require('del');
 const config = require('./config');
 
 gulp.task('clean', () => {
-  const folderDelete = [config.root.dist];
-  if (config.root.dist === './') {
-    folderDelete.push('./assets', '*.html');
+  const folderDelete = ['js', 'css', 'img', 'svg', 'fonts'].map(asset =>
+    `${config.root.dist}/${config[asset].dist}`
+  );
+
+  if (config.html.run) {
+    folderDelete.push(`${config.root.dist}/${config.html.dist}/*.html`);
   }
-  del.sync(folderDelete);
+
+  del.sync(folderDelete, {
+    force: true,
+  });
 });
